82b8b9305063c4c66a3ef4dd11074f069a0e5ee0
Maligrond <stormyweatherlight@gmail.com>
2025-08-16 21:26:31 +0200

    update

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,4 +9,15 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const books = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    author: z.string(),
+    date: z.string(),
+    rating: z.number().min(1).max(5),
+    url: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, books };
